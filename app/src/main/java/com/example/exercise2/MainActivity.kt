f6cba5131539f25a263a9cb30e7975e3f342901a
Chew Tang Hua <chewth-pm17@student.tarc.edu.my>
2019-11-29 14:49:52 +0800

    Exercise 2

--- app/src/main/java/com/example/exercise2/MainActivity.kt
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.exercise2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +18,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         CalculateBtn.setOnClickListener(){
-            val userHeight:Double = InHeight.text.toString().toDouble()
-            val userWeight:Double = InWeight.text.toString().toDouble()
-            val userBMI:Double = calculateBMI(userHeight, userWeight)
 
-            /*if (userBMI < 18.50){
+            try {
+                val userHeight: Double = InHeight.text.toString().toDouble()
+                val userWeight: Double = InWeight.text.toString().toDouble()
+                val userBMI: Double = calculateBMI(userHeight, userWeight)
+
+                /*if (userBMI < 18.50){
                 ResultImg.setImageResource(R.drawable.under)
             }else if (userBMI <= 24.90){
                 ResultImg.setImageResource(R.drawable.normal)
@@ -27,27 +32,32 @@ class MainActivity : AppCompatActivity() {
                 ResultImg.setImageResource(R.drawable.over)
             }*/
 
-            val status:String
+                val status: String
 
-            when{
-                userBMI < 18.50 ->{
-                    ResultImg.setImageResource(R.drawable.under)
-                    status = "Under"
+                when {
+                    userBMI < 18.50 -> {
+                        ResultImg.setImageResource(R.drawable.under)
+                        status = "Under"
+                    }
+
+                    userBMI <= 24.90 -> {
+                        ResultImg.setImageResource(R.drawable.normal)
+                        status = "Normal"
+                    }
+
+                    else -> {
+                        ResultImg.setImageResource(R.drawable.over)
+                        status = "Over"
+                    }
                 }
 
-                userBMI <= 24.90 ->{
-                    ResultImg.setImageResource(R.drawable.normal)
-                    status = "Normal"
-                }
+                txtResult.text = "BMI %.2f (%s)".format(userBMI, status)
+            }catch (ex:Exception){
+                val toast:Toast = Toast.makeText(this,"Invalid Input", Toast.LENGTH_LONG)
 
-                else ->{
-                    ResultImg.setImageResource(R.drawable.over)
-                    status = "Over"
-                }
+                toast.setGravity(Gravity.CENTER,0,0)
+                toast.show()
             }
-
-            txtResult.text = "BMI %.2f (%s)".format(userBMI, status)
-
         }
 
         ResetBtn.setOnClickListener(){
@@ -56,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             txtResult.setText("")
             ResultImg.setImageResource(R.drawable.empty)
 
-            InHeight.requestFocus()
+            InWeight.requestFocus()
         }
 
     }
